265d183db8944f9e9694bc34477230887e851a37
Satendra yadav <34853279+Satendra273@users.noreply.github.com>
2024-07-15 12:15:06 +0000

    converions ..

---
+++ basics/4_converion.js
@@ -0,0 +1,30 @@
+ let score = "33";
+
+ let num = Number(score);
+
+ console.log(typeof num);
+ console.log(typeof(num))
+
+ /**
+  * "33" = 33
+  * "33ab"  => NaN
+  * true =>1 false => 0
+  * 
+  */
+ let isLoggedIn =1
+ let booleanislogged =Boolean(isLoggedIn)
+ console.log(booleanislogged)
+
+
+ /**
+  * 1=>tru 0 => false
+  * " "  => false
+  * "satya "  =>true
+
+  */
+
+
+ /**
+  * 
+  * wecan conert other   sting and object etc
+  */
