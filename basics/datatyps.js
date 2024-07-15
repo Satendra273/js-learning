1aa304f032d9a7dfed9dcc0d456086507949cd8d
Satendra yadav <34853279+Satendra273@users.noreply.github.com>
2024-07-15 11:53:32 +0000

    data types

---
+++ basics/datatyps.js
@@ -0,0 +1,24 @@
+"use strict"; //treat all js code as new version
+
+//alert(2+3)//alert not works in nodejs
+//javascript standard are write on   https://tc39.es/ecma262/
+
+let  name = "satya" //string
+let age =15 //number 
+let isLoggedIn = false//boolean
+
+
+//data type
+/**
+ * number
+ * string
+ * boolean
+ * bigint
+ * null => standalone value  //example temp ==0 than 0 is also values in temp
+ * undefined 
+ * symbole => unique
+ * object
+ */
+console.log(typeof age )
+console.log(typeof null )  //output object
+console.log(typeof undefined )//output undefined
---
+++ basics/variables.js
@@ -0,0 +1,17 @@
+const accountID = 1233
+let accountEmail ="staya@1234"
+var accountPass="1234"
+accountCity ="agra" //it works but not prefferd
+let accountType ;   //undifne 
+
+
+/*   prefer not use the var 
+becouse block scope and function scope  */
+
+//accountID=111 //not allowed
+console.log(accountID);
+accountEmail="taya@123";
+accountPass="12444434"
+accountCity="dehli"
+
+console.table([accountID,accountEmail,accountPass,accountCity])
